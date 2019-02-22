68d1400e99ae9e65be32dfdc6043d152cf0ee8eb
Jin Kim <jinhkim716@gmail.com>
2019-02-21 18:05:01 -0800

    remove everything and see if it still works
    - remove everything but the FROM eisfw/ecoation-ros-app:latest, it
    should just work

--- eGTouch_drivers/Rule/eGTouch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-### Beginning: Launch eGTouchD daemon while setup boot-up ###
-/usr/bin/eGTouchD
-### End: Launch eGTouchD daemon while setup boot-up ###
-exit 0
--- run.sh
+++ run.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-OS_VERSION=$(echo $RESIN_HOST_OS_VERSION | cut -d " " -f 2)
-mod_dir="egalax_touchscreen_driver_${RESIN_DEVICE_TYPE}_${OS_VERSION}*"
-
-insmod $mod_dir/$DRIVER_NAME.ko
-result=$?
-lsmod | grep $DRIVER_NAME
-#rmmod $DRIVER_NAME
-if [ $result == 0 ]; then
-	echo "egalax touchscreen kernel module loaded!"
-else
-	echo "ERROR could not load $DRIVER_NAME touchscreen kernel!!!"
-fi
-
-xinit /xinitrc -- :0 -nocursor -dpms -s off -s noblank
+roslaunch ecoation_bringup ecoation_system.launch
