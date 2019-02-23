2d3298c351174b70901283ef75cb48150f242ba3
Jin Kim <jinhkim716@gmail.com>
2019-02-22 19:40:15 -0800

    testing running X first

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo -e "RUNNING RUN.SH"
-. /home/eis/ecoationROS/devel/setup.sh && \
-	ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}' \
-	UUID=00000000-0000-0000-iris-00000000000 \
-	roslaunch ecoation_bringup ecoation_system.launch
+#. /home/eis/ecoationROS/devel/setup.sh && \
+	#ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}' \
+	#UUID=00000000-0000-0000-iris-00000000000 \
+	#roslaunch ecoation_bringup ecoation_system.launch
+
+echo "run current process: $$"
+xinit ./xinitrc -- :0 -nocursor -dpms -s off -s noblank
